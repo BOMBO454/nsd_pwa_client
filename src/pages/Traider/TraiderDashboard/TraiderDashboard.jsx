363e4e6f548f7d406dd8ba4890fcc2c814aa9114
Pavel <p-sumbaeb@yandex.ru>
2021-05-15 19:50:59 +0300

    init

--- src/pages/Traider/TraiderDashboard/TraiderDashboard.jsx
+++ src/pages/Traider/TraiderDashboard/TraiderDashboard.jsx
@@ -3,6 +3,8 @@ import LayoutDashboard from "../../../components/layout/LayoutDashboard/LayoutDa
 import CardList from "../../../components/ui/CardList/CardList";
 import CardBalance from "../../../components/ui/CardBalance/CardBalance";
 import TokensCards from "./components/TokensCards/TokensCards";
+import Button from "../../../components/ui/Button/Button";
+import {COLOR_YELLOW} from "../../../constants/variable";
 
 export default function TraiderDashboard() {
   return (
@@ -10,6 +12,7 @@ export default function TraiderDashboard() {
       <CardList>
         <CardBalance balance={9000} />
         <TokensCards />
+        <Button background={COLOR_YELLOW}>Все токены</Button>
       </CardList>
     </LayoutDashboard>
   )
