9a05b822c1da45658738ef113e45fb519eb66da6
Pavel <p-sumbaeb@yandex.ru>
2021-05-15 19:51:10 +0300

    init

---
+++ src/constants/paths.js
@@ -0,0 +1,17 @@
+const paths = {
+  signIn: "/sign-in",
+  signUp: "/sign-up",
+  home: "/",
+
+  emitent: "/emitent/dashboard",
+  emitentTokenInfo: "/emitent/token-info",
+  emitentTokenCreate: "/emitent/create-token",
+
+  traider: "/traider/dashboard",
+  traiderTokenInfo: "/traider/token-info",
+  traiderTokensList: "/traider/tokens-list",
+
+  settings: "/user/settings",
+}
+
+export default paths
---
+++ src/pages/Emitent/EmitentTokenInfo/EmitentTokenInfo.jsx
@@ -0,0 +1,8 @@
+// import {} from "./styled";
+import LayoutDashboard from "../../../components/layout/LayoutDashboard/LayoutDashboard";
+
+export default function EmitentTokenInfo() {
+  return (
+    <LayoutDashboard>sdsfsf</LayoutDashboard>
+  )
+}
